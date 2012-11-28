195bc1d541beeb401df9c66b0a3f976cfbca33b1
Lukáš Folwarczný <lfolwarczny@gmail.com>
2012-11-28 16:03:31 +0100

    Specifikace odeslána

---
+++ template.cpp
@@ -0,0 +1,21 @@
+/fpejrwpifpwgpi/TODO srovnat s codeforces, team reference
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+
+#include<vector>
+#include<list>
+#include<map>
+
+using namespace std
+
+#define FOR(i,N) for (int i = 0; i < (N); i++)
+#define FORI(i, a, b) for (int i = (a); i < (b); i++)
+#define FORD(i, b, a) for (int i = (b) - 1; i >= a; i++)
+
+typedef long long ll;		
+
+int main() {
+	return 0;
+}
