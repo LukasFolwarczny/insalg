c1e948ab13c03d27ce1f805eb3f9059759ea2b6e
Lukáš Folwarczný <lfolwarczny@gmail.com>
2013-02-07 20:56:18 +0100

    DFS, Tarjan: Okomentováno, přidán example

--- code/graph_dfs.cpp
+++ code/graph_dfs.cpp
@@ -1,16 +1,52 @@
 #include "template.cpp"
-
 #define MaxN 100000
+// Lukas Folwarczny, 2013
+// http://atrey.karlin.mff.cuni.cz/~folwar/insalg/
 
-int T = 1, vis[MaxN], in[MaxN], out[MaxN];
+
+int N, T = 1, vis[MaxN], in[MaxN], out[MaxN];
 vector<int> E[MaxN];
+
+void dfs(int v);
+
+//Traverses oriented graph in depth-first order, N=|V|
+//E - for each vertex gives the list
+//of second vertices of oriented edges
+//Time complexity: O(|V|+|E|)
+//vis[v] determines whether v was visited
+//in[v] - time of arrival to v
+//out[v] - time of departure from v
+void dfs_graph() {
+	FOR(i,N)
+		if (!vis[i]) dfs(i);
+}
+
 
 void dfs(int v) {
 	vis[v] = 1;
 	in[v] = T++;
 	FOR(i,E[v].size())  {
-		if (!visited[E[v][i]])
+		if (!vis[E[v][i]])
 			dfs(E[v][i]);
 	}
 	out[v] = T++;	
 }
+
+
+void example() {
+	N = 9;
+	E[0] = vector<int>(2,1);
+	E[0][1] = 5;
+	E[1] = vector<int>(1,2);
+	E[2] = vector<int>(1,3);
+	E[3] = vector<int>(2,0);
+	E[3][1] = 4;
+	E[4] = vector<int>(1,5);
+	E[5] = vector<int>(1,6);
+	E[6] = vector<int>(1,5);
+	E[7] = vector<int>(1,8);
+	dfs_graph();
+	FOR(n,N) printf("vertex %d in %d out %d\n", n, in[n], out[n]);
+}
+
+int main() { example(); return 0; }
--- code/graph_tarjan-scc.cpp
+++ code/graph_tarjan-scc.cpp
@@ -1,45 +1,69 @@
 #include "template.cpp"
-
 #define MaxN 100000
 
+// Lukas Folwarczny, 2013
+// http://atrey.karlin.mff.cuni.cz/~folwar/insalg/
+
 int N;
-int T = 1, in[MaxN], out[MaxN], backlink[MAXN];
-int comp[MaxN];
-int comp_count;
+int T = 1, vis[MaxN], in[MaxN], out[MaxN], backlink[MaxN];
+int comp_count, comp[MaxN];
 vector<int> E[MaxN];
 stack<int> S;
 
+void scc(int v);
+
+//Tarjan algorithm for finding strong components of graph
+//Same notation as in graph_dfs
+//Time complexity: O(|V|+|E|)
+//Output: Vertex v is part of SCC comp[v]
 void find_sccs() {
 	FOR(i,N) {
-		if (!comp[i]) scc(i);
+		if (!vis[i]) scc(i);
 	}
 }
 
 void scc(int v) {
 	vis[v] = 1;
 	in[v] = T++;
-	lowlink[v] = in[v];
+	backlink[v] = in[v];
 	S.push(v);
 
 	FOR(i,E[v].size())  {
 		int w = E[v][i];
-		if (!in[w]) {
+		if (!vis[w]) {
 			scc(w);
-			lowlink[v] = min(lowlink[v], lowlink[w]);
+			backlink[v] = min(backlink[v], backlink[w]);
 		}
 		else if (!comp[w]) { //lies in the stack, is part of the SCC
-			lowlink[v] = min(lowlink[v], lowlink[w]);
+			backlink[v] = min(backlink[v], in[w]);
 		}
 	}
 
-	if (lowlink[v] == in[v]) { //is root of scc
+	if (backlink[v] == in[v]) { //is root of scc
 		comp_count++;
+		int w;
 		do {
-			int w = S.top(); S.pop();
+			w = S.top(); S.pop();
 			comp[w] = comp_count; 
 		}
 		while (w != v);
 	}
-	
 	out[v] = T++;	
 }
+
+void example() {
+	N = 7;
+	E[0] = vector<int>(2,1);
+	E[0][1] = 5;
+	E[1] = vector<int>(1,2);
+	E[2] = vector<int>(1,3);
+	E[3] = vector<int>(2,0);
+	E[3][1] = 4;
+	E[4] = vector<int>(1,5);
+	E[5] = vector<int>(1,6);
+	E[6] = vector<int>(1,5);
+	find_sccs();
+	FOR(n,N) printf("vertex %d in component %d\n", n, comp[n]);
+}
+
+int main() { example(); return 0; }
