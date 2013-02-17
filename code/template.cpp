ac808e386dcc71a7c0ae63581e534ded49fdd78e
Lukáš Folwarczný <lfolwarczny@gmail.com>
2013-02-17 18:39:16 +0100

    Template doplněn

--- code/template.cpp
+++ code/template.cpp
@@ -3,20 +3,28 @@
 #include<cstring>
 #include<cmath>
 
-#include<vector>
+#include<algorithm>
+#include<utility>
+#include<string>
+
+#include<deque>
 #include<list>
 #include<map>
+#include<queue>
 #include<set>
 #include<stack>
+#include<vector>
 
 using namespace std;
 
 #define FOR(i,N) for (int i = 0; i < (N); i++)
 #define FORI(i, a, b) for (int i = (a); i < (b); i++)
 #define FORD(i, b, a) for (int i = (b) - 1; i >= a; i++)
+#define DP(arg...) fprintf(stderr, ## arg) //COMPILER SPECIFIC!!!
+
 
 typedef long long ll;		
-
-int main2() {
+int main() {
+	DP("test %d %s", 1, "neco");
 	return 0;
 }
