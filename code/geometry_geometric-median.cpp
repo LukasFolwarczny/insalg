92a0a682b5b8a94b005bae9d925ad3797d657c6d
Lukáš Folwarczný <lfolwarczny@gmail.com>
2013-08-18 12:51:56 +0200

    geometric-median: minor change

--- code/geometry_geometric-median.cpp
+++ code/geometry_geometric-median.cpp
@@ -12,15 +12,17 @@ double distance(double x1, double y1, double x2, double y2) {
 void geometric_median(int N, int iterations, double* X, double* Y, double &Xmed, double
 &Ymed) {
 	double AX[2], AY[2];
-	AX[0] = 0.01120892, AY[0] = 0.01801;
+	AX[0] = (X[0]+X[1]) / 2, AY[0] = (Y[0]+Y[1])/2; // initial guess
 	FOR(it,iterations) {
 		double numerator_x = 0, numerator_y = 0, denominator = 0;
 		int t = it % 2;
 		FOR(n,N) {
 			double dist = distance(X[n], Y[n], AX[t], AY[t]);
-			numerator_x += X[n]/dist;
-			numerator_y += Y[n]/dist;
-			denominator += 1.0/dist;
+			if (dist != 0) {
+    				numerator_x += X[n]/dist;
+				numerator_y += Y[n]/dist;
+				denominator += 1.0/dist;
+			}
 		}
 		AX[1-t] = numerator_x/denominator;
 		AY[1-t] = numerator_y/denominator;
@@ -29,10 +31,12 @@ void geometric_median(int N, int iterations, double* X, double* Y, double &Xmed,
 	Ymed = AY[iterations%2];
 }
 
+// DEMO
+
 int N;
 double X[1000000], Y[1000000];
 
-void example() {
+void geometric_median_demo() {
 	scanf("%d", &N);
 	FOR(i,N) {
 		scanf("%lf %lf", &X[i], &Y[i]);
@@ -42,4 +46,6 @@ void example() {
 	printf("%lf %lf\n", RY, RY);
 }
 
-int main() { example(); return 0; }
+#ifdef RUNDEMO
+int main() { geometric_median_demo(); return 0; }
+#endif
