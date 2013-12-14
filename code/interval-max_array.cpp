3fa8e4dcec23566312ab3ab74c3ecf7801193932
Lukáš Folwarczný <lfolwarczny@gmail.com>
2013-12-14 16:40:30 +0100

    max-array: Maximum array revisited

--- code/interval-max_array.cpp
+++ code/interval-max_array.cpp
@@ -6,33 +6,18 @@
 // This array is created with the function make
 // with the complexity (time and space) O(N log N).
 // The array is immutable and computes the maximum of a given interval
-// in O(1) (it is in fact O(log log N), but this implementation uses
-// for integer constant number of operations).
-
-// The bithacks in maximum are a nice exercise.
-// I took it from: http://graphics.stanford.edu/~seander/bithacks.html
+// in O(1).
 
 /*pdf*/
-int* max_array[30];
+int max_array[30][100000];
 int N, L;
-
-static const int MultiplyDeBruijnBitPosition[32] =
-{ 0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
-  8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31 };
+int bin_log[10000]; // bin_log[i] = floor(log_2(i))
 
 //Maximum of interval [a,b)
-int maximum(int a, int b) {
+int interval_max(int a, int b) {
 	int v = b-a;
-	// bit hack
-	v |= v >> 1;
-	v |= v >> 2;
-	v |= v >> 4;
-	v |= v >> 8;
-	v |= v >> 16;
-	int r = MultiplyDeBruijnBitPosition[(unsigned int)(v * 0x07C4ACDDU) >> 	27];
-	v++;
-	v /= 2;
-	return max(max_array[r][a], max_array[r][b-v]);
+	int l = bin_log[v];
+	return max(max_array[l][a], max_array[l][b - (1 << l)]);
 }
 
 void make(int length, int* sequence) {
@@ -40,15 +25,21 @@ void make(int length, int* sequence) {
 	L = 1;
 	int p = 1;
 	while (p < length) { p *= 2; L++; }
+
+	int exp = 0, val = 1;
+	for (int i = 0; i < N; i++) {
+		if (2*val == N) val *= 2, exp++;
+		bin_log[N] = exp;
+	}
+
 	for (int l = 0; l < L; l++) {
-		max_array[l] = (int*)malloc(sizeof(int)*N);
 		if (l == 0) {
 			for (int i = 0; i < N; i++)
-			    max_array[l][i] = sequence[i];
+				max_array[l][i] = sequence[i];
 		}
 		else {
-		    for (int i = 0; i < N; i++)
-				max_array[l][i] = max(max_array[l-1][i], max_array[l-1][i+(1 << (l-1))]);
+			for (int i = 0; i < N; i++)
+				max_array[l][i] = max(max_array[l-1][i], max_array[l-1][i + (1 << (l-1))]);
 		}
 	}
 }
@@ -56,13 +47,30 @@ void make(int length, int* sequence) {
 
 void max_array_demo() {
 	int s[] = {2,3,1,2,2,10,11,-2,9,10};
-	int N = 10;
-	for (int i = 0; i < N; i++)
-	    printf("%d ", s[i]);
+	int length = 10;
+	for (int i = 0; i < length; i++)
+		printf("%d ", s[i]);
 	printf("\n");
-	make(N, s);
-	for (int i = 0; i < N; i++) for (int j = i + 1; j < N + 1; j++)
-	     printf("max [%d,%d):%d\n", i, j, maximum(i,j));
+
+	make(length, s);
+	for (int i = 0; i < length; i++) for (int j = i + 1; j < N + 1; j++)
+		printf("max [%d,%d): %d\n", i, j, interval_max(i,j));
+
+	int len, * seq;
+	printf("Enter the length of the sequence: ");
+	scanf("%d", &len);
+	seq = (int*)malloc(len * sizeof(int));
+
+	printf("\nEnter the items of the sequence: ");
+	for (int i = 0; i < len; i++) scanf("%d", &seq[i]);
+	make(len, seq);
+
+	int ret;
+	while (ret != EOF) {
+		printf("\nEnter a, b: ");
+		int a, b; ret = scanf("%d%d", &a, &b);
+		printf("\nmax [%d,%d): %d\n", a, b, interval_max(a,b));
+	}
 }
 
 #ifdef RUNDEMO
